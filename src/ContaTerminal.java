06270fd44cf91094f6ce9bb3c28e36ee6efc9073
murilofernandesgodoi <murilofernandes9250@gmail.com>
2024-08-20 14:58:58 -0400

    Resolvendo conflitos

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class ContaTerminal {
+    public static void main(String[] args) {Scanner scan = new Scanner(System.in);
+    
+    }
 }
